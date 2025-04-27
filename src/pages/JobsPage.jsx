b88f00c9bf8644afb6f0dd18598e780ad127b72d
Gustavo <lgx.0097@gmail.com>
2025-04-27 17:19:43 -0300

    refactor JobListings component to accept showAll prop for conditional rendering and update JobsPage to include JobListings

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,5 +1,9 @@
+import JobListings from "../components/JobListings";
+
 const JobsPage = () => {
-  return <h1>Jobs Page</h1>;
+  return <section className="bg-blue-50 px-4 py-6">
+    <JobListings />
+  </section>
 }
 
 export default JobsPage;
