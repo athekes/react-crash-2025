96df6488af8e35d687013d9576b2b7017d0fd801
Gustavo <lgx.0097@gmail.com>
2025-04-27 17:57:52 -0300

    feat: integrate json-server for job listings and add loading spinner
    - Updated package.json to include json-server and react-spinners dependencies.
    - Modified JobListings component to fetch job data from json-server and handle loading state.
    - Added Spinner component to display a loading indicator while fetching jobs.

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,8 +1,27 @@
-import jobsData from '../jobs.json'
+import { useState, useEffect } from 'react'
 import JobListing from './JobListing'
+import Spinner from './Spinner'
 
 const JobListings = ({ showAll = true}) => {
-  const jobListings = showAll? jobsData.jobs : jobsData.jobs.slice(0, 3)
+  const [jobs, setJobs ]      = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res  = await fetch('http://localhost:8000/jobs');
+        const data = await res.json();
+
+        setJobs(data);
+      } catch (error) {
+        console.error('Error fetching jobs:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchJobs();
+  }, []);
 
   return (
     <section className="bg-blue-50 px-4 py-10">
@@ -10,9 +29,13 @@ const JobListings = ({ showAll = true}) => {
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
           { showAll ? 'Browse Jobs': 'Recent Jobs' }
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {jobListings.map((job) => <JobListing job={job} key={job.id}/> )}
-        </div>
+        { loading ? (
+          <Spinner loading={loading}/>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            { jobs.map((job) => <JobListing job={job} key={job.id} />) }
+          </div>
+        )}
       </div>
     </section>
   )
